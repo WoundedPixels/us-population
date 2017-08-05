878c8d1ae1a8c3e60d162b23f201d98ec3439c71
ctarrington <arrington.ct@gmail.com>
2017-08-05 10:06:08 -0400

    basic sass support

---
+++ app/src/components/Button/Button.js
@@ -0,0 +1,16 @@
+// @flow
+
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends Component {
+  render() {
+    return (
+      <button className="Button">
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+export default Button;
